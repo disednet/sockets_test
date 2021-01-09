ca8a5ac23db93cecbf9346b45d0c20d27abe4d89
Dmitry Sedov <d.sedov@innopolis.ru>
2021-01-10 00:22:46 +0300

    fix

--- pipe.cpp
+++ pipe.cpp
@@ -5,63 +5,68 @@
 #include <cassert>
 
 
-//===============================================================================
-template<typename package, PackageType type>
-SendResult generatePackage(const std::string& chanel,
-  const package& data, std::vector<uint8_t>& outData) {
-  const std::size_t header_const_size = 8 + 8 + chanel.size() + sizeof(PackageType);
-  BinarySerializer ser1;
-  try {
-    ser1 << data;
-    PackageHeader header{ ser1.getSize() + header_const_size, ser1.getSize(), chanel, type };
-    BinarySerializer finalSer;
-    finalSer << header;
-    finalSer << ser1;
-    outData = finalSer.getData();
+namespace {
+  //===============================================================================
+  template<typename package, PackageType type>
+  SendResult generatePackage(const std::string& chanel,
+    const package& data, std::vector<uint8_t>& outData) {
+    const std::size_t header_const_size = 8 + 8 + chanel.size() + sizeof(PackageType);
+    BinarySerializer ser1;
+    try {
+      ser1 << data;
+      PackageHeader header{ ser1.getSize() + header_const_size, ser1.getSize(), chanel, type };
+      BinarySerializer finalSer;
+      finalSer << header;
+      finalSer << ser1;
+      auto sz = finalSer.getSize();
+      outData.resize(sz);
+      for (std::size_t i = 0; i < sz; i++)
+        outData[i] = static_cast<uint8_t>(finalSer.getData()[i]);
+    }
+    catch (std::exception& err) {
+      std::cerr << err.what() << std::endl;
+      return SendResult::SR_SERIALIZATION_FAIL;
+    }
+    return SendResult::SR_SERIALIZATION_OK;
   }
-  catch (std::exception& err) {
-    std::cerr << err.what() << std::endl;
-    return SendResult::SR_SERIALIZATION_FAIL;
+  //===============================================================================
+  CeiveResult getHeader(const char* data, unsigned int dataSize, PackageHeader& header) {
+    std::vector<uint8_t> buffer(dataSize);
+    memcpy(&buffer[0], data, dataSize);
+    BinarySerializer ser(std::move(buffer));
+    try {
+      ser >> header;
+    }
+    catch (std::runtime_error& error) {
+      std::cerr << error.what() << std::endl;
+      return CeiveResult::CR_SERIALIZATION_FAIL;
+    }
+    return CeiveResult::CR_SERIALIZATION_OK;
   }
-  return SendResult::SR_SERIALIZATION_OK;
-}
-//===============================================================================
-CeiveResult getHeader(const char* data, unsigned int dataSize, PackageHeader& header) {
-  std::vector<uint8_t> buffer(dataSize);
-  memcpy(&buffer[0], data, dataSize);
-  BinarySerializer ser(std::move(buffer));
-  try {
-    ser >> header;
-  }
-  catch (std::runtime_error& error) {
-    std::cerr << error.what() << std::endl;
+  //===============================================================================
+  template<typename T, PackageType type>
+  CeiveResult getDataFromPackage(const char* data, unsigned int dataSize, T& outData) {
+    PackageHeader header;
+    auto result = getHeader(data, dataSize, header);
+    if (result == CeiveResult::CR_SERIALIZATION_OK) {
+      if (header.data_size != 0 && header.data_size <= dataSize) {
+        std::vector<uint8_t> buffer(header.data_size);
+        const auto dataOffset = header.package_size - header.data_size;
+        memcpy(&buffer[0], data+dataOffset, header.data_size);
+        BinarySerializer ser(std::move(buffer));
+        try {
+          ser >> outData;
+        }
+        catch (std::runtime_error& err) {
+          std::cerr << err.what() << std::endl;
+          return CeiveResult::CR_SERIALIZATION_FAIL;
+        }
+        return CeiveResult::CR_SERIALIZATION_OK;
+      }
+    }
     return CeiveResult::CR_SERIALIZATION_FAIL;
   }
-  return CeiveResult::CR_SERIALIZATION_OK;
 }
-//===============================================================================
-template<typename T, PackageType type>
-CeiveResult getDataFromPackage(const char* data, unsigned int dataSize, T& outData) {
-  PackageHeader header;
-  auto result = getHeader(data, dataSize, header);
-  if (result == CeiveResult::CR_SERIALIZATION_OK) {
-    if (header.data_size != 0 && header.data_size <= dataSize) {
-      std::vector<uint8_t> buffer(dataSize);
-      memcpy(&buffer[0], data, dataSize);
-      BinarySerializer ser(std::move(buffer));
-      try {
-        ser >> outData;
-      }
-      catch (std::runtime_error& err) {
-        std::cerr << err.what() << std::endl;
-        return CeiveResult::CR_SERIALIZATION_FAIL;
-      }
-      return CeiveResult::CR_SERIALIZATION_OK;
-    }
-  }
-  return CeiveResult::CR_SERIALIZATION_FAIL;
-}
-
 
 //==========================================================================
 //
@@ -77,7 +82,7 @@ SendResult Pipe::PutEventMessage(const std::string& chanel, const std::string& e
     std::cerr << "Send event was failed.\n";
     return SendResult::SR_FAIL;
   }
-  return SendResult::SR_FAIL;
+  return SendResult::SR_OK;
 }
 
 //==========================================================================
@@ -89,7 +94,7 @@ SendResult Pipe::PutMyStruct(const std::string& chanel, const MyStruct& data) {
     std::cerr << "Send mystruct was failed.\n";
     return SendResult::SR_FAIL;
   }
-  return SendResult::SR_FAIL;
+  return SendResult::SR_OK;
 }
 
 //==========================================================================
@@ -122,7 +127,7 @@ CeiveResult Pipe::GetMyStruct(const std::string& chanel, MyStruct& data) {
 
 //==========================================================================
 void Pipe::UpdateData() {
-  const unsigned int maxHeaderSize = 120;
+  const unsigned int maxHeaderSize = 50;
   std::vector<uint8_t> inData(maxHeaderSize);
   m_socket.ReceiveWithoutPop(reinterpret_cast<char*>(&inData.at(0)), maxHeaderSize);
   PackageHeader header;
--- serializer/serializer.h
+++ serializer/serializer.h
@@ -7,21 +7,32 @@ public:
   BinarySerializer(std::vector<uint8_t> &&in) : m_buffer(std::move(in)) {
     m_iterator = m_buffer.begin();
   }
-  BinarySerializer() { m_iterator = m_buffer.begin(); }
+  BinarySerializer() { 
+  }
 
   template <typename T, typename = std::enable_if_t<std::is_integral_v<T> || std::is_floating_point_v<T> || std::is_enum_v<T>>>
   void operator<<(const T &data) {
-    if (std::distance(m_iterator, m_buffer.end()) <= sizeof(T))
-      m_buffer.resize(m_buffer.size() * 2);
+    if (m_buffer.empty()) {
+      m_buffer.resize(sizeof(T));
+      m_iterator = m_buffer.begin();
+    }
+    auto dist = std::distance(m_iterator, m_buffer.end());
+    if (dist <= sizeof(T)) {
+      auto pos = std::distance(m_buffer.begin(), m_iterator);
+      m_buffer.resize(m_buffer.size() * 2 + (sizeof(T) - dist) * 5);
+      m_iterator = m_buffer.begin() + pos;
+    }
     *(reinterpret_cast<T *>(&(*m_iterator))) = data;
     m_iterator += sizeof(T);
+    m_citerator = m_iterator;
   }
 
   template <typename T, typename = std::enable_if_t<std::is_integral_v<T> || std::is_floating_point_v<T> || std::is_enum_v<T>>>
   void operator>>(T &data) {
     if (std::distance(m_iterator, m_buffer.end()) > sizeof(T)) {
-      data = *(reinterpret_cast<T *>((*m_iterator)));
+      data = *(reinterpret_cast<T *>(&(*m_iterator)));
       m_iterator += sizeof(T);
+      m_citerator = m_iterator;
     } else {
       throw std::runtime_error("out of data range");
     }
@@ -30,36 +41,21 @@ public:
   void operator<<(const BinarySerializer &data) {
     auto dist = std::distance(m_buffer.begin(), m_iterator);
     auto currentCapacity = std::distance(m_iterator, m_buffer.end());
-    if (currentCapacity < data.m_buffer.size()) {
+    auto otherSize = data.getSize();
+    if (currentCapacity < otherSize) {
       m_buffer.resize(m_buffer.size() +
-                      (data.m_buffer.size() - currentCapacity));
+                      (otherSize - currentCapacity)*2);
     }
-    memcpy(&m_buffer[dist], &data.m_buffer[0], data.m_buffer.size());
-    m_iterator = m_buffer.begin() + dist + data.m_buffer.size();
+    memcpy(&m_buffer[dist], &data.m_buffer[0], otherSize);
+    m_iterator = m_buffer.begin() + dist + otherSize;
+    m_citerator = m_iterator;
   }
-  const std::vector<uint8_t> &getData() const { return m_buffer; }
-  const std::size_t getSize() const { return m_buffer.size(); }
+  const char* getData() const { return reinterpret_cast<const char*>(m_buffer.data()); }
+  std::size_t getSize() const { return std::distance(m_buffer.cbegin(), m_citerator); }
 
 private:
   std::vector<uint8_t> m_buffer;
   std::vector<uint8_t>::iterator m_iterator;
+  std::vector<uint8_t>::const_iterator m_citerator;
 };
-/*
-template <typename T, typename = std::enable_if_t<std::is_integral_v<T>>>
-void operator<<(BinarySerializer& sr, const T& data) {
-  if (std::distance(m_iterator, m_buffer.end()) <= sizeof(T))
-    m_buffer.resize(m_buffer.size() * 2);
-  *(reinterpret_cast<T*>(&(*m_iterator))) = data;
-  m_iterator += sizeof(T);
-}
 
-template <typename T, typename = std::enable_if_t<std::is_integral_v<T>>>
-void operator<<(T& data) {
-  if (std::distance(m_iterator, m_buffer.end()) > sizeof(T)) {
-    data = *(reinterpret_cast<T*>((*m_iterator)));
-    m_iterator += sizeof(T);
-  }
-  else {
-    throw std::runtime_error("out of data range");
-  }
-}*/
